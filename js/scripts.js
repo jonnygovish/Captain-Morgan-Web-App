f0d1b4ba2d92fb13e5eae7b7a2126d5179b9bce7
John <jonnygovish@gmail.com>
2017-08-18 16:28:57 +0300

    update the javascript scripts add code to scripts.js back-end logic

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,31 @@
 //business logic
 
-
-
-
-
-
+function range(userInput) {
+  var rangeArray = []
+  for (var number = 1; number <= userInput; number++) {
+    console.log(number)
+    if ((number % 3 === 0) && (number % 5 === 0)) {
+      rangeArray.push("captain morgan");
+    } else if (number % 3 === 0) {
+      rangeArray.push("captain");
+    } else if (number % 5 === 0) {
+      rangeArray.push("morgan")
+    } else {
+      rangeArray.push(number)
+    }
+  }
+  console.log(rangeArray);
+  return rangeArray
+}
 
 //user interface logic
 $(document).ready(function() {
-  $("form#userInput").submit(function(event) {
+  $("form").submit(function(event) {
     event.preventDefault();
     //store user input
     var user = $("#userInput").val();
     //convert user input to integer
-    var userInt = parseInt(user)
-    console.log(userInt)
+    var userInput = parseInt(user)
+    console.log(range(userInput))
   });
 });
